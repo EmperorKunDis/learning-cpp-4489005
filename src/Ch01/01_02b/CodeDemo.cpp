df87651d8b9e9945b203c48dda2128a8110d7d38
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-10-05 23:58:55 +0000

    Monogloid

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string str;
+
+    std::cout << "Enter your name: " << std::flush;
+    std::cin >> str;
+    std::cout << "Nice to meet you, " << str << "!" << std::endl;
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
